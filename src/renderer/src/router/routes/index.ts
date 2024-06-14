caaabcedb7eca18324313d70175af820556740d4
KnifeZ <knifez@qq.com>
2024-06-14 16:17:44 +0800

    fix: 隐藏登录菜单

--- src/renderer/src/router/routes/index.ts
+++ src/renderer/src/router/routes/index.ts
@@ -3,51 +3,7 @@ import { layouts, views } from '../elegant/imports'
 import { generatedRoutes } from '../elegant/routes'
 import { transformElegantRoutesToVueRoutes } from '../elegant/transform'
 
-const customRoutes: CustomRoute[] = [
-  {
-    name: 'exception',
-    path: '/exception',
-    component: 'layout.base',
-    meta: {
-      title: 'exception',
-      i18nKey: 'route.exception',
-      icon: 'ant-design:exception-outlined',
-      order: 7
-    },
-    children: [
-      {
-        name: 'exception_403',
-        path: '/exception/403',
-        component: 'view.403',
-        meta: {
-          title: 'exception_403',
-          i18nKey: 'route.exception_403',
-          icon: 'ic:baseline-block'
-        }
-      },
-      {
-        name: 'exception_404',
-        path: '/exception/404',
-        component: 'view.404',
-        meta: {
-          title: 'exception_404',
-          i18nKey: 'route.exception_404',
-          icon: 'ic:baseline-web-asset-off'
-        }
-      },
-      {
-        name: 'exception_500',
-        path: '/exception/500',
-        component: 'view.500',
-        meta: {
-          title: 'exception_500',
-          i18nKey: 'route.exception_500',
-          icon: 'ic:baseline-wifi-off'
-        }
-      }
-    ]
-  }
-]
+const customRoutes: CustomRoute[] = []
 
 export function createStaticRoutes() {
   const constantRoutes: ElegantRoute[] = []
